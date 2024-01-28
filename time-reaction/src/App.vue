078e6ade25dfd72cd0135f70638bbf3f406dd3ef
shynmah <125250436+shynmah@users.noreply.github.com>
2024-01-28 18:20:26 +0330

    biuld structur

---
+++ time-reaction/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start">play</button>
+  <block v-if="isplaying"/>
+</template>
+
+<script>
+import block from './components/Block.vue'
+export default {
+  name: 'App',
+  components: {block},
+  data(){
+    return{
+      delay: null,
+      isplaying : false
+    }
+  },
+  methods :{
+    start(){
+      this.delay= 2000 + Math.random()*5000
+      this.isplaying = true
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ time-reaction/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">click me</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .block{
+    width: 400px;
+    border-radius: 22px;
+    background-color: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+    cursor: pointer;
+ }
+
+</style>
